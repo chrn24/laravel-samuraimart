f89a69904b22a0045301d55678eb054df1715cc9
chrn24 <chrn-24@outlook.jp>
2025-06-12 21:25:54 +0900

    06122125

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -13,9 +13,15 @@ class WebController extends Controller
     public function index()
     {
         $categories=Category::all();
+
         $major_categories=MajorCategory::all();
+
         $recently_products=Product::orderBy('created_at','desc')->take(4)->get();
+
         $recommend_products=Product::where('recommend_flag',true)->take(3)->get();
-        return view('web.index',compact('major_categories','categories','recently_products','recommend_products'));
+
+         $featured_products = Product::withAvg('reviews', 'score')->orderBy('reviews_avg_score', 'desc')->take(4)->get();
+
+        return view('web.index', compact('major_categories', 'categories', 'recently_products', 'recommend_products', 'featured_products'));
     }
 }
